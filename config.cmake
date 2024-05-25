c31609ea120ad52209e4965bf19b741670f235f9
Ali Salaheldin <rated.ali.7@gmail.com>
2024-05-25 16:07:35 +0200

    update to match our conventions

--- config.cmake
+++ config.cmake
@@ -1,6 +1,6 @@
 
 # Global settings
-set(CONTEST_PREFIX cp2020)
+set(CONTEST_PREFIX "contest-")
 
 # Test settings
 set(DEFAULT_TEST_TIMEOUT 5) # seconds
--- util/compiler_options.cmake
+++ util/compiler_options.cmake
@@ -1,11 +1,12 @@
 
-if (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+if (CMAKE_CXX_COMPILER_ID  "(Clang)|(GNU)")
     add_compile_options(-Wall -Wextra -Wshadow -Wfloat-equal -Wconversion)
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
         add_compile_options(
                 -fsanitize=address,undefined
                 -fno-omit-frame-pointer
                 -fno-sanitize-recover=all
+                -D_GLIBCXX_DEBUG
         )
         add_link_options(
                 -fsanitize=address,undefined
