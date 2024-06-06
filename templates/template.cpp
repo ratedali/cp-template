6741035cb9669b1e1f7672c46d03818bbdd9dc00
Ali Salaheldin <rated.ali.7@gmail.com>
2024-06-06 11:13:18 +0200

    Update template

--- templates/template.cpp
+++ templates/template.cpp
@@ -14,9 +14,10 @@ typedef vector<vl> vvl;
 #define inf numeric_limits<int>::max()
 #define ninf numeric_limits<int>::lowest()
 #define f(i,n) for(ll i = 0; i < n; ++i)
+#define f(i,a,b) for(ll i = a; i < b; ++i)
 #define fr(i,n) for(ll i = n-1; i >= 0; --i)
-#define fi(i,a,b) for(auto i=a;i!=b;advance(it,1))
-#define fv(i,a) for(auto it=begin(a);it!=end(a);++it)
+#define fv(it,a) for(auto it=begin(a); it!=end(a); ++it)
+#define fvi(it,a,b) for(auto it=a; it!=b; advance(it,1))
 
 #define sf(x) scanf("%d", &x)
 #define sfl(x) scanf("%lld", &x)
@@ -30,11 +31,7 @@ typedef vector<vl> vvl;
 #define debug(x) \
     (cerr << #x << ": " << (x) << endl)
 #define debugit(a) \
-    {cerr << #a << " = { "; for(auto debug_x : a) cerr << debug_x << ' '; cerr << "}\n";}
-
-#define N 1'000'000
-
-ll t, n;
+    {cerr << #a << " = { "; for(auto debug_x : a) cerr << debug_x << ' '; cerr << '}' << endl;}
 
 int main() {
     ios_base::sync_with_stdio(false);
