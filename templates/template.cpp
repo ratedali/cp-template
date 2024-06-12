2a8e41f3de0921c05f9698dcd3d4eb19b8765147
Ali Salaheldin <rated.ali.7@gmail.com>
2024-06-12 17:37:21 +0200

    Update template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,40 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vii;
-typedef vector<pll> vll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
+using l = long long;
+using ii = pair<int, int>;
+using ll = pair<l, l>;
+using lll = tuple<l, l, l>;
+using vi = vector<int>;
+using vl = vector<l>;
+using vii = vector<ii>;
+using vll = vector<ll>;
+using vlll = vector<lll>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+template <typename T>
+using pq = priority_queue<T>;
+template <typename T>
+using pqr = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using uset = unordered_set<T>;
+template <typename K, typename V>
+using umap = unordered_map<K, V>;
 
-#define inf numeric_limits<int>::max()
-#define ninf numeric_limits<int>::lowest()
-#define f(i,n) for(ll i = 0; i < n; ++i)
-#define f(i,a,b) for(ll i = a; i < b; ++i)
-#define fr(i,n) for(ll i = n-1; i >= 0; --i)
+#define inf numeric_limits<l>::max() / 2
+#define ninf numeric_limits<l>::lowest()
+#define f(i, n) for (l i = 0; i < n; ++i)
+#define fi(i, a, b) for (l i = a; i < b; ++i)
+#define fr(i, n) for (l i = n - 1; i >= 0; --i)
 #define fv(it,a) for(auto it=begin(a); it!=end(a); ++it)
-#define fvi(it,a,b) for(auto it=a; it!=b; advance(it,1))
-
+#define fvi(it, a, b) for (auto it = a; it != b; advance(it, 1))
 #define sf(x) scanf("%d", &x)
 #define sfl(x) scanf("%lld", &x)
 #define pf(x) printf("%d\n", &x)
 #define pfl(x) printf("%lld\n", &x)
 
-// common memset settings
-// memset(memo, -1, sitzeof memo); // init DP memoization table
-// memset(arr, 0, sizeof arr); // clear int array
+l m, n, s, t, u, v, w, ans = 0;
+vl x, y, z, memo, pred, indeg;
+l V, E;
+vvl adj, madj, memo2;
+vvll wadj;
+vector<string> vertices;
+// memo.assign(n,-1);
+// memo2.assign(n,vl(m,-1));
 
-#define debug(x) \
-    (cerr << #x << ": " << (x) << endl)
-#define debugit(a) \
-    {cerr << #a << " = { "; for(auto debug_x : a) cerr << debug_x << ' '; cerr << '}' << endl;}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
     return 0;
 }
